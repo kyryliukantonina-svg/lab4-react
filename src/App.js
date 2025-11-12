9a46b4423be9c922c3c202b53aeaa88e7b012c16
kyryliukantonina-svg <kyryliuk.antonina@chnu.edu.ua>
2025-11-12 09:04:37 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import News from "./pages/News";
+import Profile from "./pages/Profile";
+import ProtectedRoute from "./pages/ProtectedRoute";
+
+function App() {
+  const isAuth = () => localStorage.getItem("isAuth") === "true";
+
+  return (
+    <div>
+      <nav className="topnav">
+        <div className="nav-left">
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/news" className="nav-link">News</Link>
+          <Link to="/profile" className="nav-link">Profile</Link>
+        </div>
+        <div className="nav-right">
+          {isAuth() ? (
+            <button className="btn-logout" onClick={() => { localStorage.setItem("isAuth", "false"); window.location.href = "/"; }}>
+              Logout
+            </button>
+          ) : (
+            <Link to="/login" className="nav-link">Login</Link>
+          )}
+        </div>
+      </nav>
+
+      <main className="app-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/profile" element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          } />
+          <Route path="*" element={<h2>Page not found</h2>} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
